6e68b074ceedb77305946c29607483c241e58532
Alex Babich <lex.gmx@gmail.com>
2021-04-29 17:26:39 +0300

    Updates

--- src/components/Main/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import useRoutes from '../../routes';
-
-const Main = props => {
-  const routes = useRoutes(props);
-  return <main className="main">{routes}</main>;
-};
-
-export default Main;
